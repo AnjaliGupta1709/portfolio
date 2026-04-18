7a33ecbd671b6022a0266b4d8f979eb027a0cdcb
AnjaliGupta1709 <anjaligupta6105@gmail.com>
2026-04-18 23:53:12 +0530

    updated files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import ThemeToggle from '@/components/ThemeToggle'
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
